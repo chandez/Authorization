1fd9e455c27ce30a072d5a0d64ec8c5d0d485da1
Chander Valle <chandez@gmail.com>
2018-08-08 23:08:03 -0400

    Add Identity Server

---
+++ Authorization.Ids/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Authorization.Ids.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
